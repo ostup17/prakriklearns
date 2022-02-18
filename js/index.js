815f0f6aa864882faabcb6e8b26ec6cc14ea0f79
Daniil <ostapenkodaniil948@gmail.com>
2022-02-18 11:11:57 +0600

    three learn

--- js/index.js
+++ js/index.js
@@ -1,32 +1,82 @@
+let money, time;
+function start() {
+    money = prompt('Ваш бюджет на месяц?');
+    time = prompt('Введите дату в формате YYYY-MM-DD');
+
+    while(isNaN(money) || money == "" || money == null) { //isNaN возвращает true когда не цифры
+        money = prompt('Ваш бюджет на месяц?');
+    }
+}
+start();
 
 
-// elements.addEventListener('contextmenu', messageFunction);
-// elements.addEventListener('click', messageFunction);
-// elements.addEventListener('mouseover', messageFunction);
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    saving: true
+};
 
-// let money = prompt('Ваш бюджет на месяц?');
-// let time = prompt('Введите дату в формате YYYY-MM-DD');
+function chooseExpensis() {
+    for (let i = 0; i < 2; i++) {
+        let question = +prompt('Введите обязательную статью расходов в этом месяце');
+        let quesionHow = prompt('Во сколько обойдется?');
+    
+        if ( (typeof(question) ) === 'string' && (typeof(question)) != null && (typeof(quesionHow)) != null && a != '' && b != '' && a.length < 50) {
+        appData.expenses[question] = quesionHow;
+        } else {
+            i--
+        }
+    };
+}
 
-// let appData = {
-//     budget: money,
-//     timeData: time,
-//     expenses: {},
-//     optionalExpenses: {},
-//     income: [],
-//     saving: false
-// }
+chooseExpensis();
 
-// let question = prompt('Введите обязательную статью расходов в этом месяце');
-// let quesionHow = prompt('Во сколько обойдется?');
-// let question1 = prompt('Введите обязательную статью расходов в этом месяце');
-// let quesionHow1 = prompt('Во сколько обойдется?');
-// expenses: {
-//     question : quesionHow
-// }
-// appData.expenses.question = quesionHow;
-// appData.expenses.question1 = quesionHow1;
+function detectDayBudget () {
 
-// console.log(appData.budget / 30);
+    appData.moneyPerDay = (appData.budget / 30).toFixed(1)
+    alert('Ежедневный бюджен составляет:' + appData.moneyPerDay)
+}
+
+function detectLevel () {
+
+    if (appData.moneyPerDay < 100) {
+        console.log ("Это минимальный уровень достатка!");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log ("Это средний уровень достатка!");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log ("Это высокий уровень достатка!");
+    } else {
+        console.log ("Произошла ошибка");
+    }
+}
+
+
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Kakova summa nakoplenii?"),
+            percent = +prompt("pod kakoi prozent");
+
+            appData.monthIncome = save/100/12*percent;
+            alert("Doxod vash:" + appData.monthIncome);
+    }
+}
+
+checkSavings();
+
+function chooseOptExpenses () {
+    for (let i = 0; i < 3; i++) {
+        let questionOptExpense = prompt('Статья необязательных расходов?');
+        appData.optionalExpenses[i] = questionOptExpense;
+        console.log(appData.optionalExpenses)
+    }
+}
+
+chooseOptExpenses();
+
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,29 +1,83 @@
-let message = "Hello my favorite";
-let num = 10;
+// while (i < 2) {
+//     let question = +prompt('Введите обязательную статью расходов в этом месяце');
+//     let quesionHow = prompt('Во сколько обойдется?');
+
+//     if ( (typeof(question) ) === 'string' && (typeof(question)) != null && (typeof(quesionHow)) != null && a != '' && b != '' && a.length < 50) {
+//     appData.expenses[question] = quesionHow;
+//     } else {
+//         i--
+//     }
+//     i++
+// }
+
+// do {
+//     let question = +prompt('Введите обязательную статью расходов в этом месяце');
+//     let quesionHow = prompt('Во сколько обойдется?');
+
+//     if ( (typeof(question) ) === 'string' && (typeof(question)) != null && (typeof(quesionHow)) != null && a != '' && b != '' && a.length < 50) {
+//     appData.expenses[question] = quesionHow;
+//     } else {
+//         i--
+//     }
+//     i++
+// } while (i < 2)
+
+
+// elements.addEventListener('contextmenu', messageFunction);
+// elements.addEventListener('click', messageFunction);
+// elements.addEventListener('mouseover', messageFunction);
+
+// let message = "Hello my favorite";
+// let num = 10;
 // while (num < 20) {
 //     console.log(num);
 //     num++
 // }
 
-do {
-    console.log(message)
-    num++
-}
-while (num < 20)
+// do {
+//     console.log(message)
+//     num++
+// }
+// while (num < 20)
 
-for (let i = 5; i <=10; i++) {
-    if (i === 7) {
-        break //stop all for
-        //continut пропустить 
-    }
-    console.log(i)
-}
+// for (let i = 5; i <=10; i++) {
+//     if (i === 7) {
+//         break //stop all for
+//         //continut пропустить 
+//     }
+//     console.log(i)
+// }
 
 
 // // elements.onclick = function () {
 // //     alert (message)
 // // }
 
-let messageFunction =  function() {
-    alert(message)
-}
+
+// let num = 10
+
+// function numFunc(text) {
+//     let num = 20;
+//     console.log(num)
+// }
+// numFunc('lol')
+// console.log(num)
+
+// function lol (a, b) {
+//     return (a + b)
+// }
+
+// let str = "str";
+// console.log(str.length);//3
+
+// console.log(str.toUpperCase());//STR
+// console.log(str.toLowerCase());//str
+
+
+// let twelve = "12.2";
+// console.log(Math.round(twelve)); //12
+
+// let twelve = "12.2px";
+// console.log(parseInt(twelve));//12 (not .2)
+// console.log(parseFloat(twelve));//12 (not px)
+
